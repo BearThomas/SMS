cdb3c3c5baafacd2498193a44e738147840c8713
Thomas <thomas_xiong@outlook.com>
2025-12-04 12:40:13 +0800

    Uncomment table creation and save operations

--- main/CreateLib.py
+++ main/CreateLib.py
@@ -2,22 +2,22 @@ import LibC
 
 file = LibC.Lib(r'C:\Users\thomas\Documents\GitHub\SMS\main\Date\Students.xLib')
 
-# file.crTabel("Students", ['Name', 'Age', 'Birthday', 'Group', 'Num', 'Score'])
-# file.crTabel("Groups", ['Name', 'Score', 'Head', 'Member'])
+file.crTabel("Students", ['Name', 'Age', 'Birthday', 'Group', 'Num', 'Score'])
+file.crTabel("Groups", ['Name', 'Score', 'Head', 'Member'])
 
-# file.table("Students").add(['xxx', 13, 20120403, 1, 38, 100])
-# file.table("Students").add(['xxx2', 13, 20120403, 1, 39, 100])
+file.table("Students").add(['xxx', 13, 20120403, 1, 38, 100])
+file.table("Students").add(['xxx2', 13, 20120403, 1, 39, 100])
 
 print(file.table("Students").data)
 
-# file.save()
-# file.close()
+file.save()
+file.close()
 
-# file_other = LibC.Lib('./main/Date/Other.xLib')
+file_other = LibC.Lib('./main/Date/Other.xLib')
 
-# file_other.crTabel("Announcements", ['Time', 'Note', 'People'])
-# file_other.crTabel("Lessons", ['Time', 'Subject', 'Note', 'Teacher'])
-# file_other.crTabel("Homework", ['Date', 'Note', 'Subject'])
-# file_other.crTabel("Log", ['Time', 'Type', 'Note'])
+file_other.crTabel("Announcements", ['Time', 'Note', 'People'])
+file_other.crTabel("Lessons", ['Time', 'Subject', 'Note', 'Teacher'])
+file_other.crTabel("Homework", ['Date', 'Note', 'Subject'])
+file_other.crTabel("Log", ['Time', 'Type', 'Note'])
 
-# file_other.save()
+file_other.save()
